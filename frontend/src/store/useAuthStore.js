19672d3a2d70313e0eeb35a394a9cca31b76d4e8
Subhash Kumar <subhashsonu7660@gmail.com>
2025-06-30 14:49:16 +0530

    store

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import axiosInstance from "../lib/axios";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE ==="development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "/";
+
 
 const useAuthStore = create((set, get)=>({
     authUser: null,
@@ -110,7 +111,7 @@ connectSocket: () =>{
             userId: authUser._id,
         }
     });
-    socket.connect();
+
     set({socket : socket});
 
     socket.on("getOnlineUsers",(userIds)=>{

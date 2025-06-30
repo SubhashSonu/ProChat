dc8d93ee8e38ec9f4a825afc28c6af9b97d7291c
Subhash Kumar <subhashsonu7660@gmail.com>
2025-06-30 13:06:04 +0530

    index

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,14 +18,14 @@ const PORT = process.env.PORT || 5000;
 // connect mongodb
 connectDB();
 
-//middleware
-// app.use(express.json({ limit: '5mb' })); // used to extract json data from the body
-// app.use(express.urlencoded({ limit: '5mb', extended: true }));
-// app.use(cookieParser());
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }));
+middleware
+app.use(express.json({ limit: '5mb' })); // used to extract json data from the body
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 // app.use("/api/auth",authRoutes);
 // app.use("/api/messages",messageRoutes);

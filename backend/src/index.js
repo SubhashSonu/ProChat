f803c2e7e8c11ed7f1958c97c92163171c93ebfe
Subhash Kumar <subhashsonu7660@gmail.com>
2025-06-30 14:01:44 +0530

    index

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,16 +31,15 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../../frontend/dist"); // go up 2 levels
 
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
+  app.use(express.static(frontendPath));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
-
-
-
 
 
 

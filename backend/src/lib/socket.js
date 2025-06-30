50f83d31d69bb8e157c774c92866ab491844475b
Subhash Kumar <subhashsonu7660@gmail.com>
2025-06-30 14:49:05 +0530

    socket

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,8 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors : {
-        origin: ["http://localhost:5173"]
-    }
+        origin: ["http://localhost:5173", "https://prochat-ptyf.onrender.com"],
+        credentials: true
+    },
+    
 });
 
 // used to store online users
